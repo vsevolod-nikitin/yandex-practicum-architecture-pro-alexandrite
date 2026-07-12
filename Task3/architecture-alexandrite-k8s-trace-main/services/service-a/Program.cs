45fe319f6408e5c24e72ea82d727fae5a97c01b8
Budun4eg <budun4eg@gmail.com>
2026-07-12 20:26:44 +0300

    Настройка сбора трассировок

--- Task3/architecture-alexandrite-k8s-trace-main/services/service-a/Program.cs
+++ Task3/architecture-alexandrite-k8s-trace-main/services/service-a/Program.cs
@@ -1,4 +1,19 @@
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource
+        .AddService(serviceName: "OrdersService"))
+    .WithTracing(tracing => tracing
+        .AddAspNetCoreInstrumentation()
+        .AddConsoleExporter()
+        .AddOtlpExporter(options =>
+        {
+            options.Endpoint = new Uri(builder.Configuration["TRACING_URL"] ?? throw new InvalidOperationException("TRACING_URL is not configured."));
+        }));
 
 var app = builder.Build();
 
